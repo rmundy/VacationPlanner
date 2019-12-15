2afcdbceb8cda7e83f4219942be1b40ec7fd5389
Bobby Mundy <robert.l.mundy@gmail.com>
2019-12-14 21:49:51 -0500

    Initial Commit

---
+++ VacationPlanner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  VacationPlanner
+//
+//  Created by Robert Mundy on 12/14/19.
+//  Copyright © 2019 Virtuellence. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VacationPlannerTests/VacationPlannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  VacationPlannerTests.swift
+//  VacationPlannerTests
+//
+//  Created by Robert Mundy on 12/14/19.
+//  Copyright © 2019 Virtuellence. All rights reserved.
+//
+
+import XCTest
+@testable import VacationPlanner
+
+class VacationPlannerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
